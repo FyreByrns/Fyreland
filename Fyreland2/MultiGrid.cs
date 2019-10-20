be3e1519ebacbc7d95f594c261e580b0dac675e6
Gavin White <horace5413@gmail.com>
2019-10-19 17:48:22 -0700

    Setup

---
+++ Fyreland2/MultiGrid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Fyreland2 {
+    public class MultiGrid : Grid {
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+        public int Size { get; private set; }
+
+        public MultiGrid(int rows, int columns, int size) {
+            for (int i = 0; i < rows; i++) RowDefinitions.Add(new RowDefinition() { Height = new GridLength(size, GridUnitType.Pixel) });
+            for (int i = 0; i < columns; i++) ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(size, GridUnitType.Pixel) });
+            for (int i = 0; i < rows; i++) for (int j = 0; j < columns; j++) {
+                    Border b = new Border() {
+                        BorderThickness = new Thickness(1),
+                        BorderBrush = Brushes.Black
+                    };
+
+                    Children.Add(b);
+                    SetRow(b, i);
+                    SetColumn(b, j);
+                }
+
+            Height = size * rows;
+            Width = size * columns;
+
+            Rows = rows;
+            Columns = columns;
+            Size = size;
+        }
+    }
+}
