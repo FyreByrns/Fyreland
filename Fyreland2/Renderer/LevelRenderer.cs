b15c62fceb092582815886811f5d667662ec1614
Gavin White <horace5413@gmail.com>
2019-10-25 13:19:28 -0700

    Beginning renderer.

---
+++ Fyreland2/Renderer/LevelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fyreland2.Renderer {
+    public class LevelRenderer {
+        public Tile[][,] tiles;
+        public Bitmap level;
+
+        public LevelRenderer(int width, int height) {
+            tiles = new Tile[3][,];
+            tiles[0] = new Tile[width, height];
+        }
+
+        public void Render() {
+            if (level == null) level = new Bitmap(tiles.GetLength(0) * 20, tiles.GetLength(1) * 20);
+
+
+        }
+    }
+}
